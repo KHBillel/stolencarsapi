ccf854aa341191de228e116314d4c5a12a45c76f
HBill <46976573+KHBillel@users.noreply.github.com>
2022-08-30 14:50:48 +0100

    Delete vehicules directory

--- vehicules/vehicules_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from .models import Vehicule
-from rest_framework import serializers
-
-
-class VehiculeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicule
-        fields = ['nc', 'mat', 'vbrand', 'vmodel', 'isStolen', 'short_description']
-
